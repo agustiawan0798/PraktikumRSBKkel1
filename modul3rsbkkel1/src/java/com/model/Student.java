d0c710763dea53092b461861199abef161de6733
Muhammad Agustiawan <aguztiawan4@gmail.com>
2019-11-11 00:38:30 +0700

    Modul 3
    Penambahan modul 3

---
+++ modul3rsbkkel1/src/java/com/model/Student.java
@@ -0,0 +1,57 @@
+package com.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id; 
+import javax.persistence.NamedQueries; 
+import javax.persistence.NamedQuery; 
+import javax.persistence.Table; 
+import javax.persistence.Column;
+
+@Entity
+@Table 
+@NamedQueries({@NamedQuery(name="Student.getAll",query="SELECT e FROM Student e order by e.studentId")}) 
+public class Student implements Serializable {
+
+    @Id 
+    @GeneratedValue(strategy=GenerationType.AUTO) 
+    private int studentId; 
+    @Column 
+    private String firstName; 
+    @Column 
+    private String lastName; 
+    @Column 
+    private String alamat; 
+    public Student(int studentId, String firstName, String lastName, String alamat) { 
+    this.studentId = studentId; 
+    this.firstName = firstName; 
+    this.lastName = lastName; 
+    this.alamat = alamat;
+    } 
+    public Student() { 
+    } 
+    public void setStudentId(int studentId) { 
+    this.studentId = studentId; 
+    } 
+    public int getStudentId() { 
+    return studentId; 
+    } 
+    public void setFirstName(String firstName) { 
+    this.firstName = firstName; 
+    } 
+    public String getFirstName() { 
+    return firstName; 
+    } 
+    public void setLastName(String lastName) { 
+    this.lastName = lastName; 
+    } 
+    public String getLastName() { 
+    return lastName; 
+    } 
+    public String getalamat() { 
+    return alamat; 
+    } 
+}
+
